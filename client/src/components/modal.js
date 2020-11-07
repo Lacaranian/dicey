64ec0af9b37307aaccab49026e81b16e3991743b
wuzzlefump <paulelliottwork@gmail.com>
2020-11-07 15:46:05 -0700

    modal from nav

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,6 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Die from './die'
+import dice from '../dice.json'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 
-function diceModal(){
-    return()
+function DiceModal(){
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+    return(    <>
+      <Button variant="success" onClick={handleShow}>
+      Show Dice
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        {dice.map((item)=>(
+  <Die sides={item.sides} img={item.img}/> 
+))}
+   
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>)
 }
+export default DiceModal;
--- client/src/components/navTop.js
+++ client/src/components/navTop.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
+import DiceModal from '../components/modal'
+
+
 
  function NavTop(props) {
+
+ 
+
            return(<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
     <Navbar.Brand href="#home" onClick={() => props.handlePageChange("profile")}>Dicey</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
-
-      <Nav>
-      <Nav.Link href="#action/3.1">Dice</Nav.Link>
-      </Nav>
     </Navbar.Collapse>
+    <DiceModal/>
   </Navbar>)
 
  
